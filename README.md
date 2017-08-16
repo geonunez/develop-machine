b6282b6df244c9f36b209b82f43332b043edac21
jorge.nunez <jorge.nunez@opinaiapanel.com>
2017-08-16 11:05:19 -0300

    Update system and install docker and docker-compose

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vagrant Docker Machine
+
+As MacOSX user with a old laptop, I didn't run correctly docker with docker-machine solutions (Write permission is a big problem).
+
+For that reason I create a debian machine with docker to run it completly inside a VBox. 
---
+++ docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+## Docker Compose
+dockerComposeVersion=1.15.0
+sudo curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
